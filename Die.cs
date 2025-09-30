b0082c8eeb5ba147cb3712442edab57a6fca87f7
339729139 <339729139@gotvdsb.ca>
2025-09-30 09:29:42 -0400

    Add project files.

---
+++ Die.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Die_Class
+{
+    public class Die
+    {
+        private Random _generator;
+        private int _roll;
+        private ConsoleColor _color;
+
+        public Die()
+        {
+            _generator = new Random();
+            _roll = _generator.Next(1, 7);
+            _color = ConsoleColor.Gray;
+        }
+        
+        public Die(ConsoleColor color)
+        {
+            _generator = new Random();
+            _roll = _generator.Next(1, 7);
+            _color = color;
+        }
+
+        public int Roll
+        {
+            get { return _roll; }
+            //set { _roll = value; }
+        }
+
+        public void RollDie()
+        {
+            _roll = _generator.Next(1, 7);
+        }
+
+        public override string ToString()
+        {
+            return _roll.ToString();
+        }
+
+        public void DrawRoll()
+        {
+            Console.ForegroundColor = _color;
+
+            if (_roll == 1)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|   |");
+                Console.WriteLine("| O |");
+                Console.WriteLine("|   |");
+                Console.WriteLine("-----");
+            }
+            else if (_roll == 2)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|O  |");
+                Console.WriteLine("|   |");
+                Console.WriteLine("|  O|");
+                Console.WriteLine("-----");
+            }
+            else if (_roll == 3)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|O  |");
+                Console.WriteLine("| O |");
+                Console.WriteLine("|  O|");
+                Console.WriteLine("-----");
+            }
+            else if (_roll == 4)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|O O|");
+                Console.WriteLine("|   |");
+                Console.WriteLine("|O O|");
+                Console.WriteLine("-----");
+            }
+            else if (_roll == 5)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|O O|");
+                Console.WriteLine("| O |");
+                Console.WriteLine("|O O|");
+                Console.WriteLine("-----");
+            }
+            else if (_roll == 6)
+            {
+                Console.WriteLine("-----");
+                Console.WriteLine("|O O|");
+                Console.WriteLine("|O O|");
+                Console.WriteLine("|O O|");
+                Console.WriteLine("-----");
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using Die_Class;
+namespace Topic_1_6___Summative
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Die die1 = new Die();
+            Die die2 = new Die();
+            int bank = 100;
+            int choice;
+            Console.WriteLine("Welcome to the SCAM Casino!");
+            Console.WriteLine("Press ENTER to continue...");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("What would you like to bet on?");
+            Console.WriteLine("Press 1 for Doubles (2x payout)");
+            Console.WriteLine("Press 2 for Not Doubles (0.5x payout)");
+            Console.WriteLine("Press 3 for Even Sum (1x payout)");
+            Console.WriteLine("Press 4 for Odd Sum (1x payout)");
+        }
+    }
+}
